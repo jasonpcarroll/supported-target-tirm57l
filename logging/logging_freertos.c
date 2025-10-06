5d4bab60c08ee569d8c03d9d75c308e4105ef938
czjaso <czjaso@amazon.com>
2025-10-06 15:26:14 -0700

    Setup new directory/CMake structure.

---
+++ logging/logging_freertos.c
@@ -0,0 +1,55 @@
+/* Standard includes. */
+#include <stdio.h>
+#include <stdarg.h>
+
+/* FreeRTOS-Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Logging includes. */
+#include "logging.h"
+
+/* Board Support Package includes. */
+#include "sci.h"
+
+#define LOGGING_OUTPUT_BUFFER_SIZE    512
+
+void vLoggingInit( void )
+{
+    sciInit();
+}
+
+void prvUARTSend( const char * pucInBuffer,
+                  unsigned int ulSize )
+{
+    unsigned int ulCurByte;
+
+    for( ulCurByte = 0; ulCurByte < ulSize; ++ulCurByte )
+    {
+        sciSendByte( scilinREG, ( uint8_t ) pucInBuffer[ ulCurByte ] );
+    }
+}
+
+void vLoggingPrintf( const char * pcFormatString,
+                     ... )
+{
+    static char pcOutputBuffer[ LOGGING_OUTPUT_BUFFER_SIZE ];
+    va_list xArgs;
+    int32_t lSnprintfRet;
+
+    va_start( xArgs, pcFormatString );
+
+    taskENTER_CRITICAL();
+    {
+        lSnprintfRet = vsprintf( pcOutputBuffer,
+                                 pcFormatString,
+                                 xArgs );
+
+        va_end( xArgs );
+
+        configASSERT( lSnprintfRet > 0 && lSnprintfRet < LOGGING_OUTPUT_BUFFER_SIZE );
+
+        prvUARTSend( pcOutputBuffer, lSnprintfRet );
+    }
+    taskEXIT_CRITICAL();
+}
