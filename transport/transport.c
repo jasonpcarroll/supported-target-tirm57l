0bd836022e4fcead27cd5d07e04dab3a96a1e865
czjaso <czjaso@amazon.com>
2025-11-14 09:38:52 -0800

    Minor build fixes.

--- transport/transport.c
+++ transport/transport.c
@@ -3,16 +3,28 @@
 
 /* HALCoGen includes. */
 #include "HL_emac.h"
+#include "HL_sys_vim.h"
 #include "transport.h"
 
 
+#define EMAC_RX_PULSE_INT_VIM_CHANNEL    79
+#define EMAC_TX_PULSE_INT_VIM_CHANNEL    77
+
 static uint8_t ucMACAddress[ 6 ] = { 0x00U, 0x08U, 0xEEU, 0x03U, 0xA6U, 0x6CU };
+
+extern void EMACTxIntISR(void);
+extern void EMACRxIntISR(void);
 
 TransportStatus_t Transport_Init( void )
 {
     TransportStatus_t xReturn = TRANSPORT_SUCCESS;
 
     EMACHWInit( ucMACAddress );
+
+    vimChannelMap( EMAC_RX_PULSE_INT_VIM_CHANNEL, EMAC_RX_PULSE_INT_VIM_CHANNEL, EMACRxIntISR );
+    vimChannelMap( EMAC_TX_PULSE_INT_VIM_CHANNEL, EMAC_TX_PULSE_INT_VIM_CHANNEL, EMACTxIntISR );
+    vimEnableInterrupt( EMAC_RX_PULSE_INT_VIM_CHANNEL );
+    vimEnableInterrupt( EMAC_TX_PULSE_INT_VIM_CHANNEL );
     return xReturn;
 }
 
